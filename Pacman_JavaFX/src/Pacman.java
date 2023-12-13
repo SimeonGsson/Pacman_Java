757823719ba68bbd52bb9b099762c3f056eef2ee
simeo <simeo@192.168.0.16>
2023-12-13 22:16:47 +0100

    Fourth commit

--- Pacman_JavaFX/src/Pacman.java
+++ Pacman_JavaFX/src/Pacman.java
@@ -1,36 +1,21 @@
- 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
- 
-public class Pacman extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
- Scene scene = new Scene(root, 300, 250);
+import javax.swing.JFrame;
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
- public static void main(String[] args) {
-        launch(args);
-    }
+public class Pacman extends JFrame{
+
+	public Pacman() {
+		add(new Model());
+	}
+	
+	
+	public static void main(String[] args) {
+		Pacman pac = new Pacman();
+		pac.setVisible(true);
+		pac.setTitle("Pacman");
+		pac.setSize(380,420);
+		pac.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pac.setLocationRelativeTo(null);
+		
+	}
+
 }
